c1689564824071344a4b6512648bb15e3460bb9a
SalamiTech <salamigideon01@gmail.com>
2023-06-23 05:44:11 +0000

    added constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/presentation/home.dart';
+import 'package:weather_app/presentation/Home/home.dart';
 
 void main() {
   runApp(const MyApp());
